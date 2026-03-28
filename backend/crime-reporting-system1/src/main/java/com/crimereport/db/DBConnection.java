59c192ff9bacbdd4668f3a6d70c0fa110cd87871
Koushik-0-18 <koushikkaturi02@gmail.com>
2026-03-28 14:43:01 +0530

    Switch to Supabase transaction pooler port 6543

--- backend/crime-reporting-system1/src/main/java/com/crimereport/db/DBConnection.java
+++ backend/crime-reporting-system1/src/main/java/com/crimereport/db/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:postgresql://aws-1-ap-south-1.pooler.supabase.com:5432/postgres?sslmode=require";
+    private static final String URL = "jdbc:postgresql://aws-1-ap-south-1.pooler.supabase.com:6543/postgres?sslmode=require";
     private static final String USER = "postgres.akpofvuewzunjvrhjfun";
     private static final String PASSWORD = "Koushik@2619";
 
